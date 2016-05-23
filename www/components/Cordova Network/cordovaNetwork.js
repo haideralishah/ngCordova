8e6a40a43f1fc3f35fd759f0be3fe20eaebb9799
Hailee <headeralishah@gmail.com>
2016-05-23 12:45:05 +0500

    Cordova Network Plugin Implemented

--- www/components/Cordova Network/cordovaNetwork.js
+++ www/components/Cordova Network/cordovaNetwork.js
@@ -7,10 +7,38 @@
   angular
     .module('ngCordovaEG')
     .controller('cordovaNetworkController', cordovaNetwrok);
-  function cordovaNetwrok(){
+  function cordovaNetwrok($rootScope, $cordovaNetwork){
     var cordovaNetwork = this;
+    cordovaNetwork.deviceStatus = true;
     cordovaNetwork.checkNetwork = function(){
-      
-    }
+      cordovaNetwork.type = $cordovaNetwork.getNetwork();
+      cordovaNetwork.isOnline = $cordovaNetwork.isOnline();
+      cordovaNetwork.isOffline = $cordovaNetwork.isOffline();
+      cordovaNetwork.deviceStatus = false;
+      if(cordovaNetwork.isOnline){
+        cordovaNetwork.status = 'online'
+      }
+      else if(cordovaNetwork.isOffline){
+        cordovaNetwork.status = 'offline'
+      }
+    };
+    document.addEventListener("deviceready", function () {
+
+      // listen for Online event
+      $rootScope.$on('$cordovaNetwork:online', function(event, networkState){
+        var onlineState = networkState;
+        cordovaNetwork.checkNetwork();
+        alert('Your device turns online');
+      });
+
+      // listen for Offline event
+      $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
+        var offlineState = networkState;
+        cordovaNetwork.deviceStatus = true;
+        alert('Your device turns offline');
+      });
+
+    }, false);
+
   }
 })();
